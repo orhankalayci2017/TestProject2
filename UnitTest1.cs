ad92e9f51b4bf8da025753bde9473d38c6c7edfc
orhan <orhan@kalayci.com>
2023-12-28 19:57:02 +0300

    ilk commit

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -10,7 +10,7 @@ namespace TestProject2
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            Assert.That(createFibNum.fibNum(1),Is.EqualTo(1));
         }
     }
 }
---
+++ createFibNum.cs
@@ -0,0 +1,11 @@
+﻿
+namespace TestProject2
+{
+    internal class createFibNum
+    {
+        internal static int fibNum(int v)
+        {
+            return 1;
+        }
+    }
+}
